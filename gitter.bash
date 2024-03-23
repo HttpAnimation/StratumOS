bcc8f2fc4318c52b48b7e4608a3fc34952e77227
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-22 19:45:33 -0700

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,15 +1,15 @@
 # Put gitter here
-mkdir NAME
-cd NAME
-git clone -b stable https://github.com/HttpAnimation/NAME.git
-mv NAME stable
-git clone -b blank https://github.com/HttpAnimation/NAME.git
-mv NAME blank 
-git clone -b extra https://github.com/HttpAnimation/NAME.git
-mv NAME extra
-git clone -b gh-pages https://github.com/HttpAnimation/NAME.git
-mv NAME gh-pages
-git clone -b gitter https://github.com/HttpAnimation/NAME.git
-mv NAME gitter
-git clone -b main https://github.com/HttpAnimation/NAME.git
-mv NAME main
+mkdir StratumOS
+cd StratumOS
+git clone -b stable https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS stable
+git clone -b blank https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS blank 
+git clone -b extra https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS extra
+git clone -b gh-pages https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS gh-pages
+git clone -b gitter https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS gitter
+git clone -b main https://github.com/HttpAnimation/StratumOS.git
+mv StratumOS main
